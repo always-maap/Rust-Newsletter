15f169659bcb3883ca3de1203c1192a7e9f0827c
tur1ng <mohammadali.ap.2000@gmail.com>
2022-03-23 18:47:05 +0430

    fix: use String::from instead of format

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = format!("127.0.0.1:0");
+    let address = String::from("127.0.0.1:0");
     let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
