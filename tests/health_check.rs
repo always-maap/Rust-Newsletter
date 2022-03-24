d7d2ecff8e6090566f4daec2d2d72d8ea043f026
tur1ng <mohammadali.ap.2000@gmail.com>
2022-03-24 23:11:31 +0430

    test: health_check missing data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 #[tokio::test]
-async fn health_check_works() {
+async fn health_check_success() {
     let address = spawn_app();
 
     let client = reqwest::Client::new();
@@ -14,6 +14,55 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let body = "name=mohammad&email=mohammadali.ap.2000%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscribe", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=mohammad", "missing the email"),
+        ("email=mohammadali.ap.2000%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
